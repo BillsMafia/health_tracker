fb9b54d1b2d2d76b421dd09371d502d60e5f6103
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-04 14:48:38 -0500

    exercise type controller finished

--- app/controllers/exercise_types_controller.rb
+++ app/controllers/exercise_types_controller.rb
@@ -15,15 +15,46 @@ class ExerciseTypesController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @exercise_type.update(exercise_params)
+        format.html { redirect_to @exercise_type, notice: 'Exercise Type was successfully updated.' }
+        format.json { render :show, status: :ok, location: @exercise_type }
+      else
+        format.html { render :edit }
+        format.json { render json: @exercise_type.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def create
+    @exercise_type = ExerciseType.new(exercise_params)
+
+    respond_to do |format|
+      if @exercise_type.save
+        format.html { redirect_to @exercise_type, notice: 'Exercise Type was successfully created.' }
+        format.json { render :show, status: :created, location: @exercise_type }
+      else
+        format.html { render :new }
+        format.json { render json: @exercise_type.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @exercise_type.destroy
+    respond_to do |format|
+      format.html { redirect_to exercise_type_url, notice: 'Exercise Type was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
-  def set_type
-    @exercise_type = ExerciseType.find(params[:id])
-  end
+
+  private
+    def set_type
+      @exercise_type = ExerciseType.find(params[:id])
+    end
+
+    def exercise_params
+      params.require(:exercise_type).permit(:name)
+    end
 end
